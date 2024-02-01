7de03d71ae68bc122526413772ef997f052d8489
thesckurtt <francocreepypasta@gmail.com>
2024-02-01 20:06:27 -0300

    Seeders and Factory - question and user

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+        'user_id' => 1,
+        'question_name' => "Lorem Ipsum Dolor",
+        'question_text' => fake()->text() . "?",
+        'question_alternative_01' => fake()->text() . "?",
+        'question_alternative_02' =>  fake()->text() . "?",
+        'question_alternative_03' => fake()->text() . "?",
+        'question_alternative_04' => fake()->text() . "?",
+        'question_alternative_correct' => 2,
+        ];
+    }
+}
