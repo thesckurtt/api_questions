90df7c8f2ca5d0767a0bf40915de34c9e2660b27
thesckurtt <francocreepypasta@gmail.com>
2024-02-03 09:14:56 -0300

    add dashboard template and index dashboard update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function indexDashboard(){
-        echo "hello world";
+        // echo "hello world";
+        // echo "hi";
+        // dd(auth()->user()->with('questions')->get());
+        // $questions = auth()->user()->with('questions')->get();
+        $questions = auth()->user()->questions;
+        return view('dashboard.index', [
+            "title" => "Dashboard",
+            "title_path" => "",
+        ]);
     }
 }
